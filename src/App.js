274d4fad1e81c5cfc6dba765bb9af38f86ef91fb
Mahmud2640 <mdmahmud484@gmail.com>
2022-05-23 21:21:37 +0600

    navbar and page setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Footer from "./Components/Footer/Footer";
+import Navbar from "./Components/Header/Navbar";
+import Blog from "./Pages/Blogs/Blog";
+import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello this is testing...</h1>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path = "/" element = {<Home />}></Route>
+        <Route path = "/home" element = {<Home />}></Route>
+        <Route path = "/blogs" element = {<Blog />}></Route>
+        <Route path = "/login" element = {<Login />}></Route>
+
+      </Routes>
+      <Footer />
     </div>
   );
 }
